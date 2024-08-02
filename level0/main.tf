2752c4e5ed1237d830eef4a0ad5a7e80f2730600
Vic Baker <vicbaker01@gmail.com>
2024-08-02 17:35:56 -0400

    cleaned up main.tf for level0 example.

--- level0/main.tf
+++ level0/main.tf
@@ -1,18 +1,4 @@
 provider "google" {
     project = var.gcp-project
     region  = var.region
-    # zone    = var.zone
 }
-
-
-# provider "google" {
-#     project = "${terraform.workspace}"
-#     region  = var.region
-#     # zone    = var.zone
-# }
-
-# provider "google-beta" {
-#   project = "${terraform.workspace}"
-#   region  = var.region
-#   # zone    = var.zone
-# }
